6a96972aa8b44f72ee4e73072fa8853da74ec7c5
dkahan91 <dkahan91@gmail.com>
2016-08-19 11:07:46 +0100

    Lesson 10 Homework City changer first commit - console log in JS

---
+++ Week05/lesson10/homework/starter_code/JS/index.js
@@ -0,0 +1,7 @@
+function findCity(e) {
+  e.preventDefault();
+// pseudo code: take the city entry and pull out the right background picture for the page when updated.
+  console.log("hello");
+}
+
+document.querySelector("#submit-btn").onclick = findCity;
