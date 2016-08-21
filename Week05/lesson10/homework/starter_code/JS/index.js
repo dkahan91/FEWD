bcf5ccb03fb1f513b96317864175e9004c88417c
dkahan91 <dkahan91@gmail.com>
2016-08-21 16:23:47 +0100

    Lesson 10 Commit 3 BackgroudColor not working

--- Week05/lesson10/homework/starter_code/JS/index.js
+++ Week05/lesson10/homework/starter_code/JS/index.js
@@ -8,7 +8,7 @@ function findCity() {
 // pseudo code: take the city entry and pull out the right background picture for the page when updated.
 // console.log(city);
   if(city === "london"){
-    console.log("david")
+    document.querySelector("body").style.backgroundColor="red";
   }
 else{
     console.log("ben")
@@ -22,3 +22,4 @@ document.querySelector("#submit-btn").onclick = findCity;
 
 // Questions:
 // 1. Why does else not need to have curly braces around it?
+// 2. backgroundColor doesn't work see commit 3.
