7bff371bed8e66cafa48b0da767b5b46eed0676d
dkahan91 <dkahan91@gmail.com>
2016-08-21 16:17:24 +0100

    City Picker if else function console

--- Week05/lesson10/homework/starter_code/JS/index.js
+++ Week05/lesson10/homework/starter_code/JS/index.js
@@ -1,7 +1,24 @@
-function findCity(e) {
-  e.preventDefault();
+
+
+function findCity() {
+  var city = document.querySelector("#city-type").value;
+
+  // console.log(city)
+    // e.preventDefault();
 // pseudo code: take the city entry and pull out the right background picture for the page when updated.
-  console.log("hello");
+// console.log(city);
+  if(city === "london"){
+    console.log("david")
+  }
+else{
+    console.log("ben")
+}
 }
 
+
 document.querySelector("#submit-btn").onclick = findCity;
+
+
+
+// Questions:
+// 1. Why does else not need to have curly braces around it?
