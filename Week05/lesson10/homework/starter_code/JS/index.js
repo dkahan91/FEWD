7cc279c78bfac29a7caa9ef5ff5ed0ff3e2f5a24
dkahan91 <dkahan91@gmail.com>
2016-08-21 17:57:31 +0100

    L10 Hwork Final Commit of city switch in JS

--- Week05/lesson10/homework/starter_code/JS/index.js
+++ Week05/lesson10/homework/starter_code/JS/index.js
@@ -3,7 +3,8 @@
 function findCity() {
   var cityUcaseLcase = document.querySelector("#city-type").value;
   var cityLcase = cityUcaseLcase.toLowerCase();
-  var city = cityLcase.replace(/\s+/g, '');
+  var cityspace = cityLcase.replace(/\s+/g, '');
+  var city = cityspace.trim();
   // console.log(city)
     // e.preventDefault();
 // pseudo code: take the city entry and pull out the right background picture for the page when updated.
@@ -20,12 +21,24 @@ else if(city === "austin"){
 else if(city === "losangeles"){
     document.querySelector("body").className="la";
 }
+else if(city === "la"){
+    document.querySelector("body").className="la";
+}
 else if(city === "sf"){
+    document.querySelector("body").className="sf";
+}
+else if(city === "sanfrancisco"){
     document.querySelector("body").className="sf";
 }
 else if(city === "nyc"){
     document.querySelector("body").className="nyc";
 }
+else if(city === "newyorkcity"){
+    document.querySelector("body").className="nyc";
+  }
+  else if(city === "newyork"){
+      document.querySelector("body").className="nyc";
+    }
 }
 
 
@@ -36,4 +49,5 @@ document.querySelector("#submit-btn").onclick = findCity;
 // Questions:
 // 1. Why does else not need to have curly braces around it?
 // 2. backgroundColor doesn't work see commit 3.
-// 3. Why do the variables have to be inside th function to work?
+// 3. Why do the variables have to be inside the function to work?
+// 4. How to take aout full stops if people use them? E.g. L.A.
