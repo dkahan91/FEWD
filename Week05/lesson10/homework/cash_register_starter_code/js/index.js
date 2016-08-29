4a135cf515cbf92e9a381c3172f9f24a83d00885
dkahan91 <dkahan91@gmail.com>
2016-08-29 09:05:03 +0100

    Initial start of lesson 12 hwork

--- Week05/lesson10/homework/cash_register_starter_code/js/index.js
+++ Week05/lesson10/homework/cash_register_starter_code/js/index.js
@@ -2,14 +2,24 @@
 // We want that the cash register adds up the amount entered into the entry box when submitted to appear on the register
 // and add up to the total
 //field
+  var total = 0;
+
 console.log("David");
+
+function currencyFormat(pineapple){
+   "$" + pineapple.toFixed(2);
+}
 
 function newTotal(banana){
   banana.preventDefault();
-  var newNumber = $("#newEntry").val();
-  console.log("newNumber");
-  $("#total").html("$" + newNumber);
-  // pseudocode make new line in the table with the newNumber
-  $("#entries").append("<tr><td>"+newNumber+"</td></tr>");
+  var entry = $("#newEntry").val();
+  entry = parseFloat(entry);
+  entry = entry.toFixed(2);
+  // pseudocode make new line in the table with the entry
+  $("#entries").append("<tr><td></td><td>"+"$"+entry.toFixed(2)+"</td></tr>");
+  total = total + entry;
+console.log(total);
+  $("#total").html("$"+total.toFixed(2));
+  $("#newEntry").val("");
 }
 $("#entry").submit(newTotal);
---
+++ Week06/lesson12/homework/rock_paper_scissors_starter/js/main.js
@@ -0,0 +1,69 @@
+//psedo code. We have to find a way in which we can put in all the reslts in for this game and then it will access it based on that
+
+// Rock + scissors = rock wins
+// Scissors + paper = scissors wins
+// paper + rock = paper wins
+// rock + rock = draw
+// rock + rock = draw
+// rock + rock = draw
+
+
+
+
+    //
+    //
+//     if(player1Option === 'Rock'){
+//
+//         if(player2Option ===  'Rock'){
+//             return 'DRAW';
+// }else
+
+var player1Option = "Rock"
+var player2Option = "Scissors"
+
+if (player2Option == "Scissors" && player1Option == "Rock"
+// //
+// (player2Option == "Rock" && player1Option == "Paper")
+// player2Option == "Paper" && player1Option == "Scissors"
+){
+    console.log("david");}
+
+    // console.log(player2Option == "Rock" && player1Option == "Paper");
+    //        return 'PLAYER1WINS'
+    //     }else {
+    //         return 'PLAYER2WINS'
+    //     }
+    //
+    // }else if(player1Option === 'Scissors'){
+    //
+    //       if(player2Option ===  'Rock'){
+    //           return 'PLAYER2WINS';
+    //       }else if (player2Option == 'Scissors'){
+    //          return 'DRAW'
+    //       }else {
+    //           return 'PLAYER1WINS'
+    //       }
+    //
+    //   }
+
+
+
+
+// var options = [];
+//
+// options['RockScissors'] = 'P1WIN';
+// options['RockRock'] = 'DRAW';
+// options['RockPaper'] = 'P2WIN';
+//
+//
+//
+// var deciderWinner = function(player1Option, player2Option){
+//
+//     var optionResult = options[player1Option +  player2Option]
+//     console.log(optionResult);
+// deciderWinner('Rock', 'Scissors');
+
+
+//
+//
+// player 1 = rock && player 2 = paper
